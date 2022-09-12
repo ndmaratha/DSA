16681bcb543b515a773870e9cf54bcd1e1b921bd
Nayan Pramod Desai <ndmaratha01@gmail.com>
2022-09-12 05:53:41 +0530

    mising num question on leet 268

---
+++ src/MissingNum.java
@@ -0,0 +1,29 @@
+public class MissingNum {
+    public static void main(String[] args) {
+        //https://leetcode.com/problems/missing-number/
+        int[] arr ={3,0,1,2,5};
+        System.out.println(missingNumber(arr));
+    }
+    static int missingNumber(int[] arr) {
+        int i=0;
+        while (i<arr.length){
+            int correct=arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }
+            else
+                i++;
+        }
+        for (int j = 0; j < arr.length; j++) {
+            if(arr[j] != j){
+                return j;
+            }
+        }
+        return arr.length;
+    }
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
